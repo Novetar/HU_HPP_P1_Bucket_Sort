5956a48332991bb2e47d5a7e18859e8eed45f1b4
Dominik <149472195+Novetar@users.noreply.github.com>
2025-02-09 22:56:18 +0100

    Changed a few names

--- main.cpp
+++ main.cpp
@@ -21,42 +21,41 @@ void print_vector_vector(const std::vector<std::vector<int>>& input) {
 // Sort given vector by using bucket sort
 std::vector<int> bucket_sort(std::vector<int>& vector_to_sort) {
 
-    std::vector<std::vector<int>> pos_bucket_array(10);  // Creates a two dimensional array with 10 buckets
-    std::vector<std::vector<int>> neg_bucket_array(10);
+    std::vector<std::vector<int>> pos_buckets_vector(10);  // Creates a two dimensional array with 10 buckets for positive numbers
+    std::vector<std::vector<int>> neg_buckets_vector(10);  // Creates a two dimensional array with 10 buckets for negative numbers
 
-    // Find the biggest number and get its size
-    int max_number = -999999999;
-    int max_number_size;
+    // Find the length of the longest number.
+    int max_number_size = 0;
     for (int number : vector_to_sort) {
-        if (number > max_number) {
-            max_number = number;
+        int number_size = std::to_string(number).size();
+        if (number_size > max_number_size) {
+            max_number_size = number_size;
         }
-    max_number_size = std::to_string(max_number).size();
     }
 
     for (int i = 0; i < max_number_size; i++) {
 
         // Distribution pass
         // Clears buckets
-        for (std::vector<int>& bucket : pos_bucket_array) {
+        for (std::vector<int>& bucket : pos_buckets_vector) {
             bucket.clear();
         }
         for (int number : vector_to_sort) {
             int factor = std::pow(10, i+1);
             int second_factor = std::pow(10, i);
             int focus = (number % factor - number % second_factor) / second_factor;  // (343 modulo 100 - 343 modulo 10) / 10 = 4
-            pos_bucket_array[focus].push_back(number);  // Adds the number to the corresponding bucket.
+            pos_buckets_vector[focus].push_back(number);  // Adds the number to the corresponding bucket.
         }
 
         // Debugging prints
         std::cout << std::endl;
         std::cout << "Distribution pass on index: " << i << std::endl;
-        print_vector_vector(pos_bucket_array);
+        print_vector_vector(pos_buckets_vector);
 
         // Gathering pass
         // Clears vector_to_sort
         vector_to_sort.clear();
-        for (std::vector<int> bucket: pos_bucket_array) {
+        for (std::vector<int> bucket: pos_buckets_vector) {
             for (int number : bucket) {
                 vector_to_sort.push_back(number);
             }
@@ -78,6 +77,3 @@ int main() {
 
     return 0;
 }
-
-
-
