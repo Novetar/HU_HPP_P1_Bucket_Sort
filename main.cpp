c9a8b5f080f89472d4b6f2949753b046635955d2
Dominik <149472195+Novetar@users.noreply.github.com>
2025-02-09 15:54:19 +0100

    Added better formatting for prints

--- main.cpp
+++ main.cpp
@@ -34,29 +34,41 @@ std::vector<int> bucket_sort(std::vector<int>& vector_to_sort) {
         // Distribution pass
         for (int number : vector_to_sort) {
             std::string number_as_string = std::to_string(number);  // int to string
-            std::cout << "Number as string " << number_as_string[0] << std:: endl;  // debug print
-            int integer = number_as_string[0] - '0';  // Converts char to int by subtracting ascii codes. Example 51 (3) - 48 (0) = 3
+            int integer = number_as_string[i] - '0';  // Converts char to int by subtracting ascii codes. Example 51 (3) - 48 (0) = 3
+            bucket_array[integer].push_back(number);  // Adds the number to the corresponding bucket.
+        }
 
-            bucket_array[integer].push_back(number);
+        // Debugging prints
+        std::cout << std::endl;
+        std::cout << "Distribution pass: " << i << std::endl;
+        print_vector_vector(bucket_array);
+
+        // Gathering pass
+        vector_to_sort.clear();  // Removes all elements from the vector.
+        for (std::vector<int> bucket: bucket_array) {
+            for (int number : bucket) {
+                vector_to_sort.push_back(number);
+            }
         }
     }
-
-    print_vector_vector(bucket_array);  // debug print
-
     return vector_to_sort;
 }
 
 
 int main() {
     std::vector<int> vector_to_sort = {4, 5, 3, 2, 1};
+
+    std::cout << "Array Pre Sorting" << std::endl;
     print_vector(vector_to_sort);
     bucket_sort(vector_to_sort);
+    std::cout << std::endl;
+    std::cout << "Array Post Sorting" << std::endl;
     print_vector(vector_to_sort);
 
-    // test ints
-    int test = -123;
-    std::string test_as_string = std::to_string(test);
-    std::cout << test_as_string << std::endl;
+    // // test negative ints
+    // int test = -123;
+    // std::string test_as_string = std::to_string(test);
+    // std::cout << test_as_string << std::endl;
 
     // system("pause");  // Halts the program (so the terminal doesn't close instantly)
     return 0;
