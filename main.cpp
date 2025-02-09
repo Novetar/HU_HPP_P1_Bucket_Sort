c616fb76f9caf724c7a8d03b975f6200fa758ffc
Dominik <149472195+Novetar@users.noreply.github.com>
2025-02-09 22:42:58 +0100

    Changed logic to sort numbers

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <cmath>
 
 
 void print_vector(const std::vector<int>& input) {
@@ -17,33 +18,45 @@ void print_vector_vector(const std::vector<std::vector<int>>& input) {
     }
 }
 
-
+// Sort given vector by using bucket sort
 std::vector<int> bucket_sort(std::vector<int>& vector_to_sort) {
 
-    std::vector<std::vector<int>> bucket_array(10);  // Creates a two dimensional array with the 10 buckets
+    std::vector<std::vector<int>> pos_bucket_array(10);  // Creates a two dimensional array with 10 buckets
+    std::vector<std::vector<int>> neg_bucket_array(10);
 
-    int vector_size = vector_to_sort.size();
+    // Find the biggest number and get its size
+    int max_number = -999999999;
+    int max_number_size;
+    for (int number : vector_to_sort) {
+        if (number > max_number) {
+            max_number = number;
+        }
+    max_number_size = std::to_string(max_number).size();
+    }
 
-    for (int i = 2; i >= 0; i--) {
+    for (int i = 0; i < max_number_size; i++) {
+
         // Distribution pass
-        // Clears bucket_array
-        for (std::vector<int>& bucket : bucket_array) {
+        // Clears buckets
+        for (std::vector<int>& bucket : pos_bucket_array) {
             bucket.clear();
         }
         for (int number : vector_to_sort) {
-            std::string number_as_string = std::to_string(number);  // int to string
-            int integer = number_as_string[i] - '0';  // Converts char to int by subtracting ascii codes. Example 51 (3) - 48 (0) = 3
-            bucket_array[integer].push_back(number);  // Adds the number to the corresponding bucket.
+            int factor = std::pow(10, i+1);
+            int second_factor = std::pow(10, i);
+            int focus = (number % factor - number % second_factor) / second_factor;  // (343 modulo 100 - 343 modulo 10) / 10 = 4
+            pos_bucket_array[focus].push_back(number);  // Adds the number to the corresponding bucket.
         }
 
         // Debugging prints
         std::cout << std::endl;
         std::cout << "Distribution pass on index: " << i << std::endl;
-        print_vector_vector(bucket_array);
+        print_vector_vector(pos_bucket_array);
 
         // Gathering pass
-        vector_to_sort.clear();  // Removes all elements from the vector.
-        for (std::vector<int> bucket: bucket_array) {
+        // Clears vector_to_sort
+        vector_to_sort.clear();
+        for (std::vector<int> bucket: pos_bucket_array) {
             for (int number : bucket) {
                 vector_to_sort.push_back(number);
             }
@@ -54,7 +67,7 @@ std::vector<int> bucket_sort(std::vector<int>& vector_to_sort) {
 
 
 int main() {
-    std::vector<int> vector_to_sort = {456, 536, 531, 322, 411, 788};
+    std::vector<int> vector_to_sort = {4356, 5364, 531, 322, 411, 788};
 
     std::cout << "Array Pre Sorting" << std::endl;
     print_vector(vector_to_sort);
@@ -63,12 +76,6 @@ int main() {
     std::cout << "Array Post Sorting" << std::endl;
     print_vector(vector_to_sort);
 
-    // // test negative ints
-    // int test = -123;
-    // std::string test_as_string = std::to_string(test);
-    // std::cout << test_as_string << std::endl;
-
-    // system("pause");  // Halts the program (so the terminal doesn't close instantly)
     return 0;
 }
 
