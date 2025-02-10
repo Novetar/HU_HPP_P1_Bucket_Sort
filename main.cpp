e6ff3f625e50823146b718cc46062f69f3d3dc5a
Dominik <149472195+Novetar@users.noreply.github.com>
2025-02-10 14:41:23 +0100

    works for pos and neg numbers

--- main.cpp
+++ main.cpp
@@ -57,7 +57,7 @@ void pos_bucket_sort(std::vector<int> &pos_vector_to_sort) {
         // Gathering pass
         pos_vector_to_sort.clear();
         // Convert numbers from buckets back into the vector
-        for (std::vector<int> bucket: pos_buckets) {
+        for (const std::vector<int> &bucket: pos_buckets) {
             for (int number : bucket) {
                 pos_vector_to_sort.push_back(number);
             }
@@ -71,7 +71,7 @@ void neg_bucket_sort(std::vector<int> &neg_vector_to_sort) {
     std::vector<std::vector<int>> neg_buckets(10);  // Creates a two dimensional array with 10 buckets
 
 
-    // Make numbers positive
+    // Convert the negative numbers to positive numbers
     for (int &number : neg_vector_to_sort) {
         number = number * -1;
     }
@@ -92,7 +92,7 @@ void neg_bucket_sort(std::vector<int> &neg_vector_to_sort) {
         for (const int &number : neg_vector_to_sort) {
             int factor = std::pow(10, i+1);
             int second_factor = std::pow(10, i);
-            int focus = (number % factor - number % second_factor) / second_factor;  // (343 modulo 100 - 343 modulo 10) / 10 = 4
+            int focus = 9 - (number % factor - number % second_factor) / second_factor;  // (343 modulo 100 - 343 modulo 10) / 10 = 4
             neg_buckets[focus].push_back(number);  // Adds the number to the corresponding bucket.
         }
 
@@ -100,10 +100,15 @@ void neg_bucket_sort(std::vector<int> &neg_vector_to_sort) {
         neg_vector_to_sort.clear();
         // Convert numbers from buckets back into the vector
         for (std::vector<int> bucket: neg_buckets) {
-            for (int number : bucket) {
+            for (const int &number : bucket) {
                 neg_vector_to_sort.push_back(number);
             }
         }
+    }
+
+    // Convert the numbers back to negative numbers
+    for (int &number : neg_vector_to_sort) {
+        number = number * -1;
     }
 }
 
@@ -114,7 +119,7 @@ std::vector<int> bucket_sort(std::vector<int> &vector_to_sort) {
     std::vector<int> neg_to_sort;
 
     // Sort the numbers into positive and negative
-    for (const int &number : vector_to_sort) {
+    for (int number : vector_to_sort) {
         if (number < 0) {
             neg_to_sort.push_back(number);
         } else {
@@ -122,16 +127,8 @@ std::vector<int> bucket_sort(std::vector<int> &vector_to_sort) {
         }
     }
 
-    pos_bucket_sort(pos_to_sort);
     neg_bucket_sort(neg_to_sort);
-
-
-        // // Debugging prints
-        // std::cout << std::endl;
-        // std::cout << "Distribution pass on index: " << i << std::endl;
-        // print_vector_vector(pos_buckets);
-        // print_vector_vector(neg_buckets);
-
+    pos_bucket_sort(pos_to_sort);
 
     vector_to_sort.clear();
     vector_to_sort.insert(vector_to_sort.end(), neg_to_sort.begin(), neg_to_sort.end());
@@ -141,7 +138,7 @@ std::vector<int> bucket_sort(std::vector<int> &vector_to_sort) {
 
 
 int main() {
-    std::vector<int> vector_to_sort = {4356, 5364, 531, 322, 411, 788};
+    std::vector<int> vector_to_sort = {4356, 5364, 531, 322, 411, 788, -444, -333, -22, -1};
 
     std::cout << "Array Pre Sorting" << std::endl;
     print_vector(vector_to_sort);
